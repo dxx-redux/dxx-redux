46156bf2be04a0e8221501bdadfa23b3e271a353
Ronald M. Clifford <roncli@roncli.com>
2018-02-02 13:41:47 -0800

    Fix Mac compile bug.

--- d1/main/gauges.c
+++ d1/main/gauges.c
@@ -21,6 +21,7 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 #include "inferno.h"
 #include "game.h"
