f514a4b064174e9a4c1ab2e9ac111f6f0f9d8b9b
Sirius <daniel.keymer@gmail.com>
2018-09-01 09:01:14 -0700

    D2 port for commit 9e8973ac966301709fc7fc2cf62d4931f1735c66 (roncli Jan 4, 2018)

--- d2/main/net_udp.h
+++ d2/main/net_udp.h
@@ -23,7 +23,6 @@ void net_udp_disconnect_player(int playernum);
 int net_udp_level_sync();
 void net_udp_send_mdata_direct(ubyte *data, int data_len, int pnum, int priority);
 void net_udp_send_netgame_update();
-void forward_to_observers(ubyte *data, int data_len);
 
 // Some defines
 #ifdef IPv6
