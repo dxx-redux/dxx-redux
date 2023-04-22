8d65528fb84297eea008d0b2a5ea6641e7461e7d
Arne de Bruijn <mail@arnedebruijn.nl>
2023-04-22 15:28:20 +0200

    Renamed to dxx-redux

--- d1/main/vers_id.h
+++ d1/main/vers_id.h
@@ -6,7 +6,7 @@
 #define __stringize2(X)	#X
 #define __stringize(X)	__stringize2(X)
 
-#define RH_VERSION "1.4.X7"
+#define RH_VERSION "0.1"
 
 #define DXX_VERSION_MAJOR __stringize(DXX_VERSION_MAJORi)
 #define DXX_VERSION_MINOR __stringize(DXX_VERSION_MINORi)
@@ -14,7 +14,7 @@
 
 #define BASED_VERSION "Registered v1.5 Jan 5, 1996"
 #define VERSION DXX_VERSION_MAJOR "." DXX_VERSION_MINOR "." DXX_VERSION_MICRO
-#define DESCENT_VERSION "D1X Retro " RH_VERSION 
+#define DESCENT_VERSION "D1X-Redux " RH_VERSION
 
 extern const char g_descent_version[40];
 extern const char g_descent_build_datetime[21];
