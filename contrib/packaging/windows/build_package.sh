91f4f1f033e2ca6569801f53490312a9dc18ae17
Arne de Bruijn <mail@arnedebruijn.nl>
2023-04-24 21:39:29 +0200

    Add GitHub Actions workflow for Windows
    Based on dxx-rebirth workflow by raptor <buckyballreaction@gmail.com>.

---
+++ contrib/packaging/windows/build_package.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -x
+
+GIT_HASH=$(git rev-parse --short HEAD)
+
+build_app() {
+	builddir="$1"
+    name="$2"
+    prettyname="$3"
+    
+    zipfilename="${prettyname}-win-${GIT_HASH}.zip"
+    outdir="${prettyname}"
+    tmpdir="packagetemp"
+    
+    # Have to bundle in separate directory because of case-insensitivity clashes
+    mkdir ${tmpdir}
+    cd ${tmpdir}
+    
+    mkdir ${outdir}
+    mkdir ${outdir}/demos
+    mkdir ${outdir}/missions
+    mkdir ${outdir}/screenshots
+
+    # Copy executable and libraris
+    cp ../${builddir}/${name}.exe ${outdir}/
+    
+    # Copy in .dlls the old fashioned way. This assumes all the needed DLLs are 
+    # ones from the mingw64 installation
+    ldd ${outdir}/${name}.exe |grep mingw64 |sort |cut -d' ' -f3 |while read dll; do cp "${dll}" ${outdir}/; done
+
+    # Copy in other resources
+    cp ../COPYING.txt ${outdir}/
+        
+    # zip up and output to top level dir
+    zip -r -X ../${zipfilename} ${outdir}
+    
+    cd ..
+    
+    rm -rf ${tmpdir}
+}
+
+build_app "buildd1/main" "d1x-redux" "D1X-Redux"
+build_app "buildd2/main" "d2x-redux" "D2X-Redux"
+
+# Clean up
+
