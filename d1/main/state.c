b7f0d9e241bb33aaec883b40e828ed09d07ee47c
Arne de Bruijn <mail@arnedebruijn.nl>
2023-04-24 20:09:47 +0200

    Fix hitobj_list out of bounds when loading game

--- d1/main/state.c
+++ d1/main/state.c
@@ -321,7 +321,8 @@ void state_object_rw_to_object(object_rw *obj_rw, object *obj)
 			obj->ctype.laser_info.parent_signature = obj_rw->ctype.laser_info.parent_signature;
 			obj->ctype.laser_info.creation_time    = obj_rw->ctype.laser_info.creation_time;
 			obj->ctype.laser_info.last_hitobj      = obj_rw->ctype.laser_info.last_hitobj;
-			obj->ctype.laser_info.hitobj_list[obj->ctype.laser_info.last_hitobj] = 1; // restore most recent hitobj to hitobj_list
+			if (obj->ctype.laser_info.last_hitobj >= 0)
+				obj->ctype.laser_info.hitobj_list[obj->ctype.laser_info.last_hitobj] = 1; // restore most recent hitobj to hitobj_list
 			obj->ctype.laser_info.track_goal       = obj_rw->ctype.laser_info.track_goal;
 			obj->ctype.laser_info.multiplier       = obj_rw->ctype.laser_info.multiplier;
 			break;
