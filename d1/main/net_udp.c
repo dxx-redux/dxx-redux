4e823dcf3cfaf424420ac9dd25e161bb730426b8
Arne de Bruijn <mail@arnedebruijn.nl>
2023-04-25 17:49:07 +0200

    Revert "Fix candidate for observer join bug"
    Didn't fix the bug.

--- d1/main/net_udp.c
+++ d1/main/net_udp.c
@@ -1941,12 +1941,7 @@ void net_udp_welcome_player(UDP_sequence_packet *their)
 	memset(&UDP_sync_player, 0, sizeof(UDP_sequence_packet));
 	Network_player_added = 0;
 
-	if (their->player.observer)
-	{
-		// Flush mdata before processing the new observer so we don't send
-		// them packets they can't use
-		net_udp_send_mdata(0, timer_query());
-
+	if(their->player.observer) {
 		Netgame.numobservers++;
 
 		int obsnum = 0;
