85ca3bb4769ca0a4a35a73d08344b873037d54ea
Arne de Bruijn <mail@arnedebruijn.nl>
2023-05-03 11:02:19 +0200

    Use cmake project version number in texts

--- d2/main/vers_id.h
+++ d2/main/vers_id.h
@@ -6,7 +6,15 @@
 #define __stringize2(X)	#X
 #define __stringize(X)	__stringize2(X)
 
-#define RH_VERSION "0.4"
+#define DXX_VERSION_MAJOR __stringize(DXX_VERSION_MAJORi)
+#define DXX_VERSION_MINOR __stringize(DXX_VERSION_MINORi)
+#define DXX_VERSION_MICRO __stringize(DXX_VERSION_MICROi)
+
+#if DXX_VERSION_MICROi
+#define RH_VERSION DXX_VERSION_MAJOR "." DXX_VERSION_MINOR "." DXX_VERSION_MICRO
+#else
+#define RH_VERSION DXX_VERSION_MAJOR "." DXX_VERSION_MINOR
+#endif
 
 #define D2XMAJOR __stringize(DXX_VERSION_MAJORi)
 #define D2XMINOR __stringize(DXX_VERSION_MINORi)
