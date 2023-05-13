dxx-redux/dxx-redux

commit 8d0193f651d30604d8694ed5e46315780060f6d1

GaussAmmoStyle ported to D1

d1/main/net_udp.c
@@ -3024,6 +3024,7 @@ void net_udp_send_game_info(struct _sockaddr sender_addr, ubyte info_upid, ubyte
 		buf[len] = Netgame.HomingNormQuick; len++;
 		buf[len] = Netgame.AllowCustomModelsTextures; len++;
 		buf[len] = Netgame.ReducedFlash; len++;
+		buf[len] = Netgame.GaussAmmoStyle; len++;
 
 		if(info_upid == UPID_SYNC) {
 			PUT_INTEL_INT(buf + len, player_tokens[to_player]); len += 4; 
@@ -3254,6 +3255,7 @@ int net_udp_process_game_info(ubyte *data, int data_len, struct _sockaddr game_a
 		Netgame.HomingNormQuick = data[len]; len++;
 		Netgame.AllowCustomModelsTextures = data[len]; len++;
 		Netgame.ReducedFlash = data[len]; len++;
+		Netgame.GaussAmmoStyle = data[len]; len++;
 
 		if (Netgame.host_is_obs) {
 			multi_make_player_ghost(0);
@@ -3752,6 +3754,8 @@ static int opt_homing_update_rate;
 static int opt_homing_norm_quick;
 static int opt_allow_custom_models_textures;
 static int opt_reduced_flash;
+static int opt_gauss_duplicating, opt_gauss_depleting, opt_gauss_steady_recharge, opt_gauss_steady_respawn;
+
 #ifdef USE_TRACKER
 static int opt_tracker;
 #endif
@@ -3783,9 +3787,9 @@ void net_udp_more_game_options ()
 	char PrimDupText[80],SecDupText[80],SecCapText[80]; 
 	char HomingUpdateRateText[80];
 #ifdef USE_TRACKER
-	newmenu_item m[38];
+	newmenu_item m[44];
 #else
- 	newmenu_item m[37];
+	newmenu_item m[43];
 #endif
 
 	snprintf(packstring,sizeof(char)*4,"%d",Netgame.PacketsPerSec);
@@ -3839,6 +3843,18 @@ void net_udp_more_game_options ()
 	opt_spawn_preview = opt; 
 	m[opt].type = NM_TYPE_RADIO; m[opt].text = "Preview"; m[opt].value = Netgame.SpawnStyle == SPAWN_STYLE_PREVIEW; m[opt].group = 0; opt++;
 		
+	m[opt].type = NM_TYPE_TEXT; m[opt].text = ""; opt++;
+
+	m[opt].type = NM_TYPE_TEXT; m[opt].text = "Vulcan Ammo Style"; opt++;
+	opt_gauss_duplicating = opt;
+	m[opt].type = NM_TYPE_RADIO; m[opt].text = "D2 (Duplicating)"; m[opt].value = Netgame.GaussAmmoStyle == GAUSS_STYLE_DUPLICATING; m[opt].group = 1; opt++;
+	opt_gauss_depleting = opt;
+	m[opt].type = NM_TYPE_RADIO; m[opt].text = "Original (Depleting)"; m[opt].value = Netgame.GaussAmmoStyle == GAUSS_STYLE_DEPLETING; m[opt].group = 1; opt++;
+	opt_gauss_steady_recharge = opt;
+	m[opt].type = NM_TYPE_RADIO; m[opt].text = "Steady (Recharging)"; m[opt].value = Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RECHARGING; m[opt].group = 1; opt++;
+	opt_gauss_steady_respawn = opt;
+	m[opt].type = NM_TYPE_RADIO; m[opt].text = "Steady (Respawning)"; m[opt].value = Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RESPAWNING; m[opt].group = 1; opt++;
+
 
 	m[opt].type = NM_TYPE_TEXT; m[opt].text = ""; opt++;
 
@@ -4036,16 +4052,15 @@ int net_udp_more_options_handler( newmenu *menu, d_event *event, void *userdata 
 			{
 				Netgame.HomingUpdateRate=menus[opt_homing_update_rate].value + 15;
 				sprintf( menus[opt_homing_update_rate].text, "Homing Update Rate: %d", Netgame.HomingUpdateRate);
-			} else if (citem == opt_spawn_no_invul) {
-				Netgame.SpawnStyle = SPAWN_STYLE_NO_INVUL;
-			} else if (citem == opt_spawn_short_invul) {
-				Netgame.SpawnStyle = SPAWN_STYLE_SHORT_INVUL;
-			} else if (citem == opt_spawn_long_invul) {
-				Netgame.SpawnStyle = SPAWN_STYLE_LONG_INVUL;
-			} else if (citem == opt_spawn_preview) {
-				Netgame.SpawnStyle = SPAWN_STYLE_PREVIEW;
+			} else if (citem == opt_gauss_duplicating) {
+				Netgame.GaussAmmoStyle = GAUSS_STYLE_DUPLICATING;
+			}  else if (citem == opt_gauss_depleting) {
+				Netgame.GaussAmmoStyle = GAUSS_STYLE_DEPLETING;
+			}  else if (citem == opt_gauss_steady_recharge) {
+				Netgame.GaussAmmoStyle = GAUSS_STYLE_STEADY_RECHARGING;
+			}  else if (citem == opt_gauss_steady_respawn) {
+				Netgame.GaussAmmoStyle = GAUSS_STYLE_STEADY_RESPAWNING;
 			}
-
 
 			break;
 			
@@ -4278,6 +4293,7 @@ int net_udp_setup_game()
 	Netgame.HomingNormQuick = 0;
 	Netgame.AllowCustomModelsTextures = 0;
 	Netgame.ReducedFlash = 0;
+	Netgame.GaussAmmoStyle = GAUSS_STYLE_DEPLETING;
 
 #ifdef USE_TRACKER
 	Netgame.Tracker = 1;

d1/main/collide.c
@@ -1212,6 +1212,13 @@ void drop_player_eggs_remote(object *playerobj, ubyte remote)
 		{
 			min_vulcan_ammo = VULCAN_WEAPON_AMMO_AMOUNT /2;
 		}
+		if( (Game_mode & GM_MULTI) && ! (Game_mode & GM_MULTI_COOP)) {
+			if( (Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RECHARGING) ||
+			    (Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RESPAWNING) )
+			{
+				vulcan_ammo = min_vulcan_ammo;
+			}
+		}
 		if (vulcan_ammo < min_vulcan_ammo)
 			vulcan_ammo = min_vulcan_ammo;	//make sure gun has at least as much as a powerup
 		objnum = maybe_drop_primary_weapon_egg(playerobj, VULCAN_INDEX);
@@ -1235,15 +1242,37 @@ void drop_player_eggs_remote(object *playerobj, ubyte remote)
 		drop_missile_1_or_4(playerobj,HOMING_INDEX, remote);
 		drop_missile_1_or_4(playerobj,CONCUSSION_INDEX, remote);
 
-		//	If player has vulcan ammo, but no vulcan cannon, drop the ammo.
-		if (!(Players[playerobj->id].primary_weapon_flags & HAS_VULCAN_FLAG)) {
-			int	amount = Players[playerobj->id].primary_ammo[VULCAN_INDEX];
-			if (amount > 200) {
-				amount = 200;
+		if( (! (Game_mode & GM_MULTI))  ||
+			  (  Game_mode & GM_MULTI_COOP)   ||
+			  ( Netgame.GaussAmmoStyle == GAUSS_STYLE_DEPLETING)) {
+			if (!(Players[playerobj->id].primary_weapon_flags & HAS_VULCAN_FLAG)) {
+				int	amount = Players[playerobj->id].primary_ammo[VULCAN_INDEX];
+				if (amount > 200) {
+					amount = 200;
+				}
+				while (amount > 0) {
+					call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_VULCAN_AMMO);
+					amount -= VULCAN_AMMO_AMOUNT;
+				}
 			}
-			while (amount > 0) {
+		} else if (Netgame.GaussAmmoStyle == GAUSS_STYLE_DUPLICATING) {
+			if (! (Players[playerobj->id].primary_weapon_flags & HAS_FLAG(VULCAN_INDEX)) ) {
+				int	amount = Players[playerobj->id].primary_ammo[VULCAN_INDEX];
+				if (amount > 200) {
+					amount = 200;
+				}
+				while (amount > 0) {
+					call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_VULCAN_AMMO);
+					amount -= VULCAN_AMMO_AMOUNT;
+				}
+			}
+		} else if ( (Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RECHARGING) ||
+			        (Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RESPAWNING) )
+		{
+			int boxes = VulcanAmmoBoxesOnBoard[playerobj->id];
+			while (boxes > 0) {
 				call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_VULCAN_AMMO);
-				amount -= VULCAN_AMMO_AMOUNT;
+				boxes -= 1;
 			}
 		}
 

d1/main/gameseq.c
@@ -410,6 +410,8 @@ void init_player_stats_new_ship(ubyte pnum)
 	Players[pnum].homing_object_dist = -F1_0;
 
 	RespawningConcussions[pnum] = 0; 
+	VulcanAmmoBoxesOnBoard[pnum] = 0;
+	VulcanBoxAmmo[pnum] = 0;
 
 	digi_kill_sound_linked_to_object(Players[pnum].objnum);
 	

d1/main/laser.c
@@ -1228,6 +1228,22 @@ void do_laser_firing_player(void)
 				else
 					plp->primary_ammo[Players[Player_num].primary_weapon] -= ammo_used;
 
+				if( (Game_mode & GM_MULTI) &&
+				    (! (Game_mode & GM_MULTI_COOP)) &&
+				    Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RESPAWNING )
+				{
+					if(VulcanBoxAmmo[Player_num] > 0) {
+						VulcanBoxAmmo[Player_num] -= ammo_used;
+
+						if(VulcanBoxAmmo[Player_num] <=
+							(VulcanAmmoBoxesOnBoard[Player_num] - 1) * VULCAN_AMMO_AMOUNT) {
+
+							maybe_drop_net_powerup(POW_VULCAN_AMMO);
+							VulcanAmmoBoxesOnBoard[Player_num] -= 1;
+						}
+					}
+				}
+
 			auto_select_weapon(0);		//	Make sure the player can fire from this weapon.
 
 			if (Game_mode & GM_MULTI)

d1/main/multi.c
@@ -2249,7 +2249,15 @@ multi_do_player_explode(const ubyte *buf)
 	Players[pnum].secondary_ammo[SMART_INDEX] = buf[count];         count++;
 	Players[pnum].secondary_ammo[MEGA_INDEX] = buf[count];          count++;
 	Players[pnum].secondary_ammo[PROXIMITY_INDEX] = buf[count]; count++;
-	Players[pnum].primary_ammo[VULCAN_INDEX] = GET_INTEL_SHORT(buf + count); count += 2;
+
+	if( (Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RECHARGING) ||
+		(Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RESPAWNING) )
+	 {
+		VulcanAmmoBoxesOnBoard[pnum] = GET_INTEL_SHORT(buf + count); count += 2;
+	} else {
+		Players[pnum].primary_ammo[VULCAN_INDEX] = GET_INTEL_SHORT(buf + count); count += 2;
+	}
+
 	Players[pnum].flags = GET_INTEL_INT(buf + count);               count += 4;
 
 	multi_powcap_adjust_remote_cap (pnum);
@@ -3227,8 +3235,15 @@ multi_send_player_explode(char type)
 	multibuf[count++] = (char)Players[Player_num].secondary_ammo[SMART_INDEX];
 	multibuf[count++] = (char)Players[Player_num].secondary_ammo[MEGA_INDEX];
 	multibuf[count++] = (char)Players[Player_num].secondary_ammo[PROXIMITY_INDEX];
-	PUT_INTEL_SHORT(multibuf+count, Players[Player_num].primary_ammo[VULCAN_INDEX] );
-	count += 2;
+	if( (Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RECHARGING) ||
+		(Netgame.GaussAmmoStyle == GAUSS_STYLE_STEADY_RESPAWNING) )
+	{
+		PUT_INTEL_SHORT(multibuf+count, (short)(VulcanAmmoBoxesOnBoard[Player_num]) );
+		count += 2;
+	} else {
+		PUT_INTEL_SHORT(multibuf+count, Players[Player_num].primary_ammo[VULCAN_INDEX] );
+		count += 2;
+	}
 	PUT_INTEL_INT(multibuf+count, Players[Player_num].flags );
 	count += 4;
 

d1/main/multi.h
@@ -170,6 +170,11 @@ for_each_multiplayer_command(enum {, define_multiplayer_command, });
 #define SPAWN_STYLE_LONG_INVUL 2
 #define SPAWN_STYLE_PREVIEW 3
 
+#define GAUSS_STYLE_DUPLICATING 0
+#define GAUSS_STYLE_DEPLETING 1
+#define GAUSS_STYLE_STEADY_RECHARGING 2
+#define GAUSS_STYLE_STEADY_RESPAWNING 3
+
 #define for_each_netflag_value(VALUE)	\
 	VALUE(NETFLAG_DOLASER, "Laser upgrade")	\
 	VALUE(NETFLAG_DOQUAD, "Quad lasers")	\
@@ -503,6 +508,7 @@ typedef struct netgame_info
 	ubyte						HomingNormQuick;
 	ubyte						AllowCustomModelsTextures;
 	ubyte						ReducedFlash;
+	ubyte						GaussAmmoStyle;
 } __pack__ netgame_info;
 
 extern int Host_is_obs; // Reminder for host only that they are an observer.  Do not set for other players or observers.

d1/main/net_udp.h
@@ -48,7 +48,7 @@ void net_udp_send_mdata_direct(ubyte *data, int data_len, int pnum, int priority
 #define UPID_GAME_INFO_REQ_SIZE			 13
 #define UPID_GAME_INFO_LITE_REQ_SIZE		 11
 #define UPID_GAME_INFO				  3 // Packet containing all info about a netgame.
-#define UPID_GAME_INFO_SIZE			(5 + 4*2 + 364 + (NETGAME_NAME_LEN+1) + (MISSION_NAME_LEN+1) + ((MAX_PLAYERS+4)*(CALLSIGN_LEN+1+1)) + 18*12)
+#define UPID_GAME_INFO_SIZE			(5 + 4*2 + 365 + (NETGAME_NAME_LEN+1) + (MISSION_NAME_LEN+1) + ((MAX_PLAYERS+4)*(CALLSIGN_LEN+1+1)) + 18*12)
 #define UPID_GAME_INFO_LITE_REQ			  4 // Requesting lite info about a netgame. Used for discovering games.
 #define UPID_GAME_INFO_LITE			  5 // Packet containing lite netgame info.
 #define UPID_GAME_INFO_LITE_SIZE		 (31 + (NETGAME_NAME_LEN+1) + (MISSION_NAME_LEN+1))

d1/main/player.c
@@ -16,6 +16,8 @@ vms_matrix Last_orient; // Saved orientation of observer prior to following a pl
 fix Last_real_update; // How long ago in seconds the observed player's real position got updated.
 vms_vector Real_pos; // The observed player's position.
 vms_matrix Real_orient; // The observed player's orientation.
+int VulcanAmmoBoxesOnBoard[MAX_PLAYERS];
+int VulcanBoxAmmo[MAX_PLAYERS];
 
 void player_rw_swap(player_rw *p, int swap)
 {

commit a676d6703509b22339bc5d3f9bd1841db3427943

Fix spawn style option in D1
Saving the spawn style option got accidentially taken out when merging
the GaussAmmoStyle option from D2 to D1.

Fixes: 8d0193f ("GaussAmmoStyle ported to D1")

d1/main/net_udp.c
@@ -4075,6 +4075,14 @@ int net_udp_more_options_handler( newmenu *menu, d_event *event, void *userdata 
 			{
 				Netgame.HomingUpdateRate=menus[opt_homing_update_rate].value + 20;
 				sprintf( menus[opt_homing_update_rate].text, "Homing Update Rate: %d", Netgame.HomingUpdateRate);
+			} else if (citem == opt_spawn_no_invul) {
+				Netgame.SpawnStyle = SPAWN_STYLE_NO_INVUL;
+			} else if (citem == opt_spawn_short_invul) {
+				Netgame.SpawnStyle = SPAWN_STYLE_SHORT_INVUL;
+			} else if (citem == opt_spawn_long_invul) {
+				Netgame.SpawnStyle = SPAWN_STYLE_LONG_INVUL;
+			} else if (citem == opt_spawn_preview) {
+				Netgame.SpawnStyle = SPAWN_STYLE_PREVIEW;
 			} else if (citem == opt_gauss_duplicating) {
 				Netgame.GaussAmmoStyle = GAUSS_STYLE_DUPLICATING;
 			}  else if (citem == opt_gauss_depleting) {