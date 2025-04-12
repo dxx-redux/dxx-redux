643b866bda05904caabd2bf602031b7ca0e9a2c7
Arne de Bruijn <mail@arnedebruijn.nl>
2025-04-12 11:35:52 +0200

    Support C23

--- d2/main/object.c
+++ d2/main/object.c
@@ -1969,9 +1969,9 @@ void object_move_one( object * obj )
 
 		#ifndef RELEASE
 		case CT_SLEW:
-			if ( keyd_pressed[KEY_PAD5] ) slew_stop( obj );
+			if ( keyd_pressed[KEY_PAD5] ) slew_stop();
 			if ( keyd_pressed[KEY_NUMLOCK] ) 		{
-				slew_reset_orient( obj );
+				slew_reset_orient();
 			}
 			slew_frame(0 );		// Does velocity addition for us.
 			break;
