20fbb1d4a6fa5b12aab23e2daa1dd562735fd2ea
Arne de Bruijn <mail@arnedebruijn.nl>
2025-05-01 08:05:19 +0200

    Optimize ogl draw_flat / draw_tmap
    Remove heap allocation.

--- d1/arch/ogl/ogl.c
+++ d1/arch/ogl/ogl.c
@@ -73,6 +73,8 @@
 #define sinf(a) sin(a)
 #endif
 
+#define MAX_VERTS 128
+
 unsigned char *ogl_pal=gr_palette;
 
 int last_width=-1,last_height=-1;
@@ -698,10 +700,13 @@ bool g3_draw_poly(int nv,g3s_point **pointlist)
 {
 	int c, index3, index4;
 	float color_r, color_g, color_b, color_a;
-	GLfloat *vertex_array, *color_array;
+	GLfloat vertex_array[MAX_VERTS * 3], color_array[MAX_VERTS * 4];
 
-	MALLOC(vertex_array, GLfloat, nv*3);
-	MALLOC(color_array, GLfloat, nv*4);
+	if (nv > MAX_VERTS)
+		Error("Too many vertices %d", nv);
+
+	//MALLOC(vertex_array, GLfloat, nv*3);
+	//MALLOC(color_array, GLfloat, nv*4);
 
 	r_polyc++;
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -735,8 +740,8 @@ bool g3_draw_poly(int nv,g3s_point **pointlist)
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_COLOR_ARRAY);
 
-	d_free(vertex_array);
-	d_free(color_array);
+	//d_free(vertex_array);
+	//d_free(color_array);
 
 	return 0;
 }
@@ -751,13 +756,18 @@ void draw_tmap_flat(grs_bitmap *bm,int nv,g3s_point **vertlist){
 
 extern void (*tmap_drawer_ptr)(grs_bitmap *bm,int nv,g3s_point **vertlist);
 
+
 /*
  * Everything texturemapped (walls, robots, ship)
  */ 
 bool g3_draw_tmap(int nv,g3s_point **pointlist,g3s_uvl *uvl_list,g3s_lrgb *light_rgb,grs_bitmap *bm)
 {
 	int c, index2, index3, index4;
-	GLfloat *vertex_array, *color_array, *texcoord_array, color_alpha = 1.0;
+	GLfloat vertex_array[MAX_VERTS * 3], color_array[MAX_VERTS * 4], texcoord_array[MAX_VERTS * 2];
+	GLfloat color_alpha = 1.0;
+
+	if (nv > MAX_VERTS)
+		Error("Too many vertices: %d", nv);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
@@ -778,9 +788,9 @@ bool g3_draw_tmap(int nv,g3s_point **pointlist,g3s_uvl *uvl_list,g3s_lrgb *light
 		return 0;
 	}
 
-	MALLOC(vertex_array, GLfloat, nv*3);
-	MALLOC(color_array, GLfloat, nv*4);
-	MALLOC(texcoord_array, GLfloat, nv*2);
+	//MALLOC(vertex_array, GLfloat, nv*3);
+	//MALLOC(color_array, GLfloat, nv*4);
+	//MALLOC(texcoord_array, GLfloat, nv*2);
 
 	for (c=0; c<nv; c++) {
 		index2 = c * 2;
@@ -818,9 +828,9 @@ bool g3_draw_tmap(int nv,g3s_point **pointlist,g3s_uvl *uvl_list,g3s_lrgb *light
 	glDisableClientState(GL_COLOR_ARRAY);
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 
-	d_free(vertex_array);
-	d_free(color_array);
-	d_free(texcoord_array);
+	//d_free(vertex_array);
+	//d_free(color_array);
+	//d_free(texcoord_array);
 
 	return 0;
 }
@@ -831,17 +841,20 @@ bool g3_draw_tmap(int nv,g3s_point **pointlist,g3s_uvl *uvl_list,g3s_lrgb *light
 bool g3_draw_tmap_2(int nv, g3s_point **pointlist, g3s_uvl *uvl_list, g3s_lrgb *light_rgb, grs_bitmap *bmbot, grs_bitmap *bmovl, int orient)
 {
 	int c, index2, index3, index4;
-	GLfloat *vertex_array, *color_array, *texcoordovl_array;
+	GLfloat vertex_array[MAX_VERTS * 3], color_array[MAX_VERTS * 4], texcoordovl_array[MAX_VERTS * 2];
 #ifdef OGL_MERGE
-	GLfloat *texcoordbot_array;
+	GLfloat texcoordbot_array[MAX_VERTS * 2];
 	int super = bmovl->bm_flags & BM_FLAG_SUPER_TRANSPARENT;
 #endif
 
-	MALLOC(vertex_array, GLfloat, nv*3);
-	MALLOC(color_array, GLfloat, nv*4);
-	MALLOC(texcoordovl_array, GLfloat, nv*2);
+	if (nv > MAX_VERTS)
+		Error("Too many vertices: %d", nv);
+
+	//MALLOC(vertex_array, GLfloat, nv*3);
+	//MALLOC(color_array, GLfloat, nv*4);
+	//MALLOC(texcoordovl_array, GLfloat, nv*2);
 #ifdef OGL_MERGE
-	MALLOC(texcoordbot_array, GLfloat, nv*2);
+	//MALLOC(texcoordbot_array, GLfloat, nv*2);
 #endif
 
 #ifndef OGL_MERGE
@@ -941,11 +954,11 @@ bool g3_draw_tmap_2(int nv, g3s_point **pointlist, g3s_uvl *uvl_list, g3s_lrgb *
 	r_tpolyc++;
 
 #ifdef OGL_MERGE
-	d_free(texcoordbot_array);
+	//d_free(texcoordbot_array);
 #endif
-	d_free(texcoordovl_array);
-	d_free(color_array);
-	d_free(vertex_array);
+	//d_free(texcoordovl_array);
+	//d_free(color_array);
+	//d_free(vertex_array);
 
 	return 0;
 }
