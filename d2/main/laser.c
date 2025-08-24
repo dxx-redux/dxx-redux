840a09b6aa53c750f82576e27c34c562e4e3482a
SiriusTR <21977335+SiriusTR@users.noreply.github.com>
2025-08-25 11:40:32 +1200

    Split the weapon damage values in Laser_create_new too

--- d2/main/laser.c
+++ d2/main/laser.c
@@ -716,15 +716,22 @@ int Laser_create_new( vms_vector * direction, vms_vector * position, int segnum,
 		} else if (obj->id == LASER_ID_L4) {
 			obj->shields = 13 * F1_0;
 		} else if (obj->id == LASER_ID_L5) {
-			obj->shields = 13.25 * F1_0;
+			obj->shields = 13 * F1_0;
 		} else if (obj->id == LASER_ID_L6) {
-			obj->shields = 13.5 * F1_0;
+			obj->shields = 13 * F1_0;
 		} else if (obj->id == SPREADFIRE_ID) {
 			obj->shields = 10 * F1_0;
 		} else if (obj->id == FUSION_ID) {
 			// Note: This actually just cancels out the multi_damage_scale applied to fusion,
 			// but it works, so no real reason to change it right now.
 			obj->shields = 60 * F1_0;
+		}
+	}
+	if ((Game_mode & GM_MULTI) && Netgame.RebalancedWeapons) {
+		if (obj->id == LASER_ID_L5) {
+			obj->shields = 13.25 * F1_0;
+		} else if (obj->id == LASER_ID_L6) {
+			obj->shields = 13.5 * F1_0;
 		} else if (obj->id == SUPERPROX_ID) {
 			// Smart mine; down from 30
 			obj->shields = 10 * F1_0;
