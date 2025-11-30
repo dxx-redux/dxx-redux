b104be7a74621aca2428d7efc47d8af19448b067
Arne de Bruijn <mail@arnedebruijn.nl>
2025-11-30 15:17:51 +0100

    Reduce sound latency
    Reduce sound buffer size to 512 sample frames (stereo samples).
    This is hopefully a good compromise, 256 sounds better, but
    recommended value is 1024.

--- d1/arch/sdl/digi_mixer.c
+++ d1/arch/sdl/digi_mixer.c
@@ -35,11 +35,7 @@
 #define MIX_OUTPUT_CHANNELS	2
 
 #define MAX_SOUND_SLOTS 64
-#if !((defined(__APPLE__) && defined(__MACH__)) || defined(macintosh))
-#define SOUND_BUFFER_SIZE 2048
-#else
-#define SOUND_BUFFER_SIZE 1024
-#endif
+#define SOUND_BUFFER_SIZE 512 // sample frames, so 44100/512 = 86 updates/second
 #define MIN_VOLUME 10
 
 static int digi_initialised = 0;
