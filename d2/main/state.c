0208e9e31556288c4517e6f0c97a05b931c913e9
vlo <vlo@kr4d.de>
2026-07-12 21:33:58 +0200

    Fix reactor gunpoint calculation
    * Game crashes when loading a save game where no recator is in due to a
      boss

--- d2/main/state.c
+++ d2/main/state.c
@@ -396,8 +396,10 @@ void state_object_rw_to_object(object_rw *obj_rw, object *obj)
 			// gun points of reactor now part of the object but of course not saved in object_rw and overwritten due to reset_objects(). Let's just recompute them.
 			int i = 0;
 			reactor *reactor = get_reactor_definition(obj->id);
-			for (i=0; i<reactor->n_guns; i++)
-				calc_controlcen_gun_point(reactor, obj, i);
+			if (obj->type == OBJ_CNTRLCEN) {
+			  for (i=0; i<reactor->n_guns; i++)
+				  calc_controlcen_gun_point(reactor, obj, i);
+			}
 			break;
 		}
 	}
